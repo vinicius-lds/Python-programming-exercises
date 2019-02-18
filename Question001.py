0b30c45db54f9ac04a3c7493a306756d649e5b62
vinicius-lds <36527676+vinicius-lds@users.noreply.github.com>
2019-02-17 21:19:43 -0300

    Update Question001.py

--- Question001.py
+++ Question001.py
@@ -6,7 +6,7 @@ HIGHER_LIMIT = 3200
 
 # Resolution
 final_list = list()
-for x in range(LOWER_LIMIT, HIGHER_LIMIT, 1):
-    if x % DIVISIBLE == 0 and x & NON_DIVISIBLE != 0:
+for x in range(LOWER_LIMIT, HIGHER_LIMIT):
+    if x % DIVISIBLE == 0 and x % NON_DIVISIBLE != 0:
         final_list.append(x)
 print(final_list)
